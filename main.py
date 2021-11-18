3d0569a079250eb467a4f9c13d2d799d73711012
Rohan Kumar <rohancalgary14@gmail.com>
2021-11-18 01:59:24 -0700

    added callbacks and reading tensorboard scalars

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from model import Model
 import pytorch_lightning as pl
 import matplotlib.pyplot as plt
 
+from pytorch_lightning import callbacks
+from tensorboard.backend.event_processing import event_accumulator
+
 # dataloader = PawpularityDataModule().train_dataloader()
 # images, labels = next(iter(dataloader))
 
@@ -19,10 +22,27 @@ import matplotlib.pyplot as plt
 datamodule = PawpularityDataModule()
 model = Model()
 
-logger = pl.loggers.TensorBoardLogger("tb_logs", "swinv1", default_hp_metric=False)
+logger = pl.loggers.TensorBoardLogger("tb_logs", "trash", default_hp_metric=False)
+early_stopping_cb = callbacks.EarlyStopping(monitor="val_rmse")
+model_cb = callbacks.ModelCheckpoint(
+    filename='{epoch}-{val_loss:.2f}',
+    monitor="val_rmse",
+    save_top_k=1,
+    mode="min",
+    save_last=False,
+)
 trainer = pl.Trainer(
     logger=logger,
-    max_epochs=20,
+    max_epochs=2,
+    callbacks=[early_stopping_cb, model_cb],
     gpus=1,
 )
 trainer.fit(model, datamodule=datamodule)
+
+# print scalars when we need to calculate cv score
+event_acc = event_accumulator.EventAccumulator('tb_logs/swinv1/version_1')
+event_acc.Reload()
+
+tag = "val_rmse"
+for scalar_event in event_acc.Scalars(tag):
+    print(scalar_event.value)
